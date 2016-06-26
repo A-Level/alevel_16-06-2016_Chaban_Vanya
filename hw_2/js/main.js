b4ff98d90f3db8a6ef1949f0f92ff6099cdef069
Malamand <zanusilker@gmail.com>
2016-06-26 16:38:46 +0300

    Footer and popular products

---
+++ hw_2/js/main.js
@@ -0,0 +1,11 @@
+  $('.sl').slick({
+      autoplay: "true",
+      autoplaySpeed: 15000,
+      speed: 350,
+  });
+  $('.sg').slick({
+      autoplay: "true",
+      autoplaySpeed: 8000,
+      speed: 250,
+      arrows: "false"
+  });
